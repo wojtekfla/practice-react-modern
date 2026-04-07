df19f1cbe292cf8519b53979fc363a88384df6da
Wojtek <wojtek.wfla@gmail.com>
2026-04-07 09:16:51 +0200

    complete excercise 3

--- 03/components/App.js
+++ 03/components/App.js
@@ -1,15 +1,15 @@
-// ./src/components/App.js
 import React from 'react';
 import Box from './Box';
 import Div from './Div';
+import { TextContext } from '../context';
 
-const App = () => {
-    return (
-        <section>
-            <Box />
-            <Div />
-        </section>
-    );
-};
+const App = () => (
+  <section>
+    <Box />
+    <TextContext.Provider value="sibling">
+      <Div />
+    </TextContext.Provider>
+  </section>
+);
 
 export default App;
--- 03/components/Div.js
+++ 03/components/Div.js
@@ -1,19 +1,25 @@
-// ./src/components/Div.js
 import React from 'react';
+import { ColorContext, TextContext } from '../context';
 
-const Div = () => {
-    return (
-        <div>
-            <h1>??</h1>
+const Div = () => (
+  <ColorContext.Consumer>
+    {(color) => (
+      <TextContext.Consumer>
+        {(text) => (
+          <div style={{ border: `1px solid ${color}` }}>
+            <h1>{text}</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia, justo et posuere viverra,
-                ligula turpis egestas nisi, id iaculis augue ex non nisi. Class aptent taciti sociosqu ad litora
-                torquent per conubia nostra, pedr inceptos himenaeos. Etiam at ex orci. Praesent hendrerit diam quis
-                tempor cursus. Aenean vitae porttitor purj us, eget luctus nisi. Mauris ex nibh, aliquet venenatis
-                sagittis vitae, lobortis vitae ex. Donec congue dui leo, at auctor est laoreet gravida.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia, justo et posuere viverra,
+              ligula turpis egestas nisi, id iaculis augue ex non nisi. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra, pedr inceptos himenaeos. Etiam at ex orci. Praesent hendrerit diam quis tempor cursus.
+              Aenean vitae porttitor purj us, eget luctus nisi. Mauris ex nibh, aliquet venenatis sagittis vitae,
+              lobortis vitae ex. Donec congue dui leo, at auctor est laoreet gravida.
             </p>
-        </div>
-    );
-};
+          </div>
+        )}
+      </TextContext.Consumer>
+    )}
+  </ColorContext.Consumer>
+);
 
 export default Div;
